344f217a4e585731bfe86fad887a2a7abdf8daa6
ulysseyson <ulysseyson57@gmail.com>
2022-08-19 13:12:47 +0900

    Move to wsl2

--- boj_1966/main.cpp
+++ boj_1966/main.cpp
@@ -1,29 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
-priority_queue< pair<int, int> > pq;
+queue< pair <int, int>> q;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+
     int t;
     cin >> t;
     while (t--)
     {
         int n, m;
         cin >> n >> m;
+        int doc[101];
         for(int i=0; i<n; i++)
         {
-            int tmp;
-            cin >> tmp;
-            pq.push(make_pair(tmp, i));
+            cin >> doc[i];
+            q.push(make_pair(doc[i], i));
         }
         int seq = 1;
-        while(pq.top().second != m)
+        // 빌떄까지 한번에 한번 출력
+        while(!q.empty())
         {
-            pq.pop();
+            // q를 전부 탐색하면서 중요도 최대값을 탐색
+            int max = 0;
+            for(int i=0; i<q.size(); i++)
+            {
+                if(max < q.front().first){
+                    max = q.front().first;
+                }
+            }
+            //최대값이 나올때까지 pop & push
+            while(q.front().first==max){
+                pair<int, int> tmp = q.front();
+                q.pop();
+                q.push(tmp);
+            }
+            if(q.front().second == m) break;
+            q.pop();
             seq++;
         }
         cout << seq << "\n";
-        
     }
     
 }
